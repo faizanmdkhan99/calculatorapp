a2bd32b2e357fff732f29616c231f7dd9914306f
MOHAMMED FAIZAN KHAN <1JB17IS044@sjbit.edu.in>
2021-06-04 10:02:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using calculateapp;
+namespace calculatorapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the numbers");
+            int x=Convert.ToInt32(Console.ReadLine());
+            int y=Convert.ToInt32(Console.ReadLine());
+            calculate obj=new calculate();
+            obj.add(x,y);
+            obj.subtract(x,y);
+            obj.product(x,y);
+            obj.divide(x,y);
+
+        }
+    }
+}
